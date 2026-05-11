66e53d09e018d748784c4c1c4f2982bd6fcad3be
raddevus <1radeutsch1@gmail.com>
2026-05-10 22:24:37 -0400

    Initial creation of project & repo

---
+++ GetValue/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+await GetValue();
+ async Task<int> GetValue(string request)
+{
+
+   if(_cache.TryGetValue(request, out var fromCache))
+   {
+      return fromCache;
+   }
+   int newValue = await GetValueFromServer(request);
+   return newValue;
+}
+
